29d80cf85eb4edfb44b0c3078822969b5859ad4b
dulquer9 <73124813+dulquer9@users.noreply.github.com>
2021-03-09 00:59:20 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @NickxFury
 
 class Translation(object):
     
     START_TEXT = """<b>Hai {}!!</b>
-<i>Am Just A Simple Hand Auto Filter Bot_ Bot For Searching Files From Channel...
-
-Just Sent Any Text I Will Search In All Connected Chat And Reply You With The Message link
-
-You Can Even Connected To 3 Channels At A Time...</i>"""    
+<i>Sorry.. This Bot Can Only Be Used in Movie Club Group!...</i>"""    
     
     HELP_TEXT = """<i><u>Usage Guide</u></i>
 
@@ -30,11 +26,11 @@ Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code>Adv Auto Filter Bot</code>
 
-<b>➥ Creator</b> : <b><i><a herf="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a herf="https://t.me/NickxFury">Nick Fury</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a herf="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
 
-<b>➥ Source Code</b> : <i><a herf="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">GitHub</a></i>
+<b>➥ Source Code</b> : <i><a herf="https://t.me/kurachkanjiedukkatte">Click Here</a></i>
 """
