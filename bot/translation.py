6b92ded28bdf9ccab9f567e6bbc63ecef52cb523
dulquer9 <73124813+dulquer9@users.noreply.github.com>
2021-03-09 19:38:08 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -24,7 +24,7 @@ Now You Are All Set And Ready To Go...
 Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code>Adv Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code>Mr. X Bot</code>
 
 <b>➥ Creator</b> : <b><i><a herf="https://t.me/NickxFury">Nick Fury</a></i></b>
 
